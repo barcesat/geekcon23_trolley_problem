fc89959cf15dc5091babe05920937911d6652dc2
barcesat <barcesat@gmail.com>
2023-09-22 14:42:59 +0300

    stabilized ui

--- app_nicegui.py
+++ app_nicegui.py
@@ -1,31 +1,22 @@
-# from flask import Flask, render_template, request, redirect, url_for
 import os
 from nicegui import ui
 import random
-# app = Flask(__name__)
 
 # Set the paths to your folders A and B
 folder_a_path = 'static/images/A/'
 folder_b_path = 'static/images/B/'
 
-
-
-# @app.route('/')
-def index():
-    # Get the list of image files from both folders
-    image_files_a = [filename for filename in os.listdir(folder_a_path) if filename.endswith((".jpg", ".png"))]
-    image_files_b = [filename for filename in os.listdir(folder_b_path) if filename.endswith((".jpg", ".png"))]
-
+def refresh_choices(random_index, image_files_a, image_files_b):
     # Check if there are images in both folders
     if not image_files_a or not image_files_b:
         print("No image files found in one or both of the specified folders.")
     else:
         # Randomly select an index
-        random_index = random.randint(0, min(len(image_files_a), len(image_files_b)) - 1)
-
+        print("selecting choice index: "+str(random_index))
         # Select the corresponding images
         selected_image_a = image_files_a[random_index]
         selected_image_b = image_files_b[random_index]
+        print("A: ", selected_image_a, "B: ", selected_image_b)
 
         # Read and display the text file for the selected image
         text_a_file_path = os.path.join(folder_a_path, os.path.splitext(selected_image_a)[0] + ".txt")
@@ -42,46 +33,50 @@ def index():
                 text_b = file.read()
         else:
             text_b = "No text available."
-
-
-        with ui.grid(columns=2):
-            ui.label(text_a)
-            ui.label(text_b)
-
-            with ui.interactive_image('static/images/A/'+selected_image_a).style('width: 75%'):
-                # ui.button(on_click=lambda: ui.notify('A'), icon='thumb_up') \
-                #     .props('flat fab color=white') \
-                #     .classes('absolute bottom-0 left-0 m-2')
-                ui.label(text_a).classes('absolute-top text-subtitle1 text-center')
-
-            # ui.image('static/images/B/'+selected_image_b)
-            
-            with ui.interactive_image('static/images/B/'+selected_image_b).style('width: 75%'):
-                # ui.button(on_click=lambda: ui.notify('B'), icon='thumb_up') \
-                #     .props('flat fab color=white') \
-                #     .classes('absolute bottom-0 left-0 m-2')
-                ui.label(text_b).classes('absolute-top text-subtitle1 text-center')
         
-        
-        dark = ui.dark_mode()
-        ui.label('Switch mode:')
-        ui.button('Dark', on_click=dark.enable)
-        ui.button('Light', on_click=dark.disable)
-    # return render_template('index.html', image_a=url_for('static', filename='images/A/' + image_a),
-                        #    image_b=url_for('static', filename='images/B/' + image_b),
-                        #    text_a=text_a, text_b=text_b)
+        return selected_image_a, selected_image_b, text_a, text_b
 
-# @app.route('/select_image', methods=['POST'])
-def select_image():
+def display_index(random_index, selected_image_a, selected_image_b, text_a, text_b):
+        grid.clear()
+        with grid:
+            with ui.interactive_image('static/images/A/'+selected_image_a).style('max-height: 300px w-1/2').props('flat bordered') as image_holder_a:
+                a_button = ui.button(text=text_a, on_click=lambda: select_image('A')).classes('absolute top-10 absolute-center text-h4')
+
+            with ui.interactive_image('static/images/B/'+selected_image_b).style('max-height: 300px w-1/2').props('flat bordered')  as image_holder_b:
+                b_button = ui.button(text=text_b, on_click=lambda: select_image('B')).classes('absolute top-10 absolute-center text-h4')
+
+        with ui.row():
+            ui.label('index: ' +str(random_index)).classes('vertical-bottom')
+
+def select_image(selection):
+    ui.notify(selection)
     # selected_image = request.form.get('selected_image')
-
+    random_index = random.randint(0, min(len(image_files_a), len(image_files_b)) - 1)
+    selected_image_a, selected_image_b, text_a, text_b = refresh_choices(random_index, image_files_a, image_files_b)
     # Handle user's selection (e.g., load the selected image, update text, etc.)
     # Implement your logic here based on 'selected_image'
-
-    return redirect(url_for('index'))
+    return display_index(random_index, selected_image_a, selected_image_b, text_a, text_b)
 
 if __name__ in {"__main__", "__mp_main__"}:
-    # app.run(debug=True)
-    # ui.label('Hello NiceGUI!')
+    # Get the list of image files from both folders
+    image_files_a = [filename for filename in os.listdir(folder_a_path) if filename.endswith((".jpg", ".png"))]
+    image_files_b = [filename for filename in os.listdir(folder_b_path) if filename.endswith((".jpg", ".png"))]
+    print("got", str(len(image_files_a))," files on A")
+    print("got", str(len(image_files_b))," files on B")
+    with ui.row().classes('w-full justify-center'):
+        ui.label('GeekCon 2023 Trolley Problem Game').classes('text-h4')
+    ui.separator()
+    first_random = random.randint(0, min(len(image_files_a), len(image_files_b)) - 1)
+    print(first_random)
+    selected_image_a, selected_image_b, text_a, text_b = refresh_choices(first_random, image_files_a, image_files_b)
+    grid = ui.grid(columns=2)#.classes('w-full') #.classes('w-full absolute-center')
+    display_index(first_random, selected_image_a, selected_image_b, text_a, text_b)
+    
+    with ui.row().classes('w-full justify-center'):
+        # ui.label('GeekCon 2023 Trolley Problem Game').classes('text-h4')
+        dark = ui.dark_mode()
+        with ui.grid(columns=3).classes('absolute-bottom'):
+            ui.label('Switch mode:')
+            ui.button('Dark', on_click=dark.enable)
+            ui.button('Light', on_click=dark.disable)
     ui.run(port=5000)
-    index()
