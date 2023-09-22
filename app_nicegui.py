4fcae11df1e9cac6e12d39a19e309acb3cdcd24e
Ziv Barcesat <barcesat@gmail.com>
2023-09-22 21:33:12 +0100

    added timer slider

--- app_nicegui.py
+++ app_nicegui.py
@@ -7,6 +7,16 @@ from trolley_control import ArduinoController
 # Set the paths to your folders A and B
 folder_a_path = 'static/images/A/'
 folder_b_path = 'static/images/B/'
+descision_timeout_increment = 0.1
+descision_timeout = 10.0
+
+def update_timer():
+    slider.set_value((slider.value + 0.1) % 10.1)
+    print (slider.value)
+    if slider.value >= 10.0:
+        slider.set_value(0.0)
+        countdown.deactivate()
+        select_image("NONE")
 
 def refresh_choices(random_index, image_files_a, image_files_b):
     # Check if there are images in both folders
@@ -39,22 +49,41 @@ def refresh_choices(random_index, image_files_a, image_files_b):
         return selected_image_a, selected_image_b, text_a, text_b
 
 def display_index(random_index, selected_image_a, selected_image_b, text_a, text_b):
-        grid.clear()
-        with grid:
-            with ui.interactive_image('static/images/A/'+selected_image_a, on_mouse=lambda: select_image('A')).style('max-height: 300px w-1/2').props('flat bordered') as image_holder_a:
-                a_button = ui.button(text=text_a, on_click=lambda: select_image('A'), color="blue").classes('text-h3 w-full absolute-top justify-center')
+    # timer = ui.timer(0.1, lambda: slider.set_value((slider.value + 0.01) % 1.0))
+    # ui.notify('NONE').bind_value_to(timer, 'active')
+    # if slider.value == 1.0:
+    #     select_image('NONE')
+    # if countdown._is_canceled:
+    #     countdown.
+    
+    grid.clear()
+    with grid:
+        with ui.interactive_image('static/images/A/'+selected_image_a, on_mouse=lambda: select_image('A')).style('max-height: 300px w-1/2').props('flat bordered') as image_holder_a:
+            a_button = ui.button(text=text_a, on_click=lambda: select_image('A'), color="blue").classes('text-h3 w-full absolute-top justify-center')
 
-            with ui.interactive_image('static/images/B/'+selected_image_b, on_mouse=lambda: select_image('B')).style('max-height: 300px w-1/2').props('flat bordered')  as image_holder_b:
-                b_button = ui.button(text=text_b, on_click=lambda: select_image('B'), color="red").classes('text-h3 w-full absolute-top justify-center')
+        with ui.interactive_image('static/images/B/'+selected_image_b, on_mouse=lambda: select_image('B')).style('max-height: 300px w-1/2').props('flat bordered')  as image_holder_b:
+            b_button = ui.button(text=text_b, on_click=lambda: select_image('B'), color="red").classes('text-h3 w-full absolute-top justify-center')
 
         with ui.row():
             ui.label('index: ' +str(random_index)).classes('vertical-bottom')
 
 def select_image(selection):
-    ui.notify(selection)
+    
     print("user has selected: ",selection)
     random_index = random.randint(0, min(len(image_files_a), len(image_files_b)) - 1)
     selected_image_a, selected_image_b, text_a, text_b = refresh_choices(random_index, image_files_a, image_files_b)
+    
+    if selection == "A":
+        ui.notify("You have selected: "+ text_a, multi_line=True, classes='multi-line-notification')
+    elif selection == "A":
+        ui.notify("You have selected: "+ text_b, multi_line=True, classes='multi-line-notification')
+ 
+    if selection == "NONE":
+        ui.notify("Abstention is just as worse as taking an action!", type='negative',classes='multi-line-notification')
+        # countdown.deactivate()
+        countdown.activate()
+        return display_index(random_index, selected_image_a, selected_image_b, text_a, text_b)
+    
     if arduino.port_opened_successfully:
         try:
             if selection == "A":
@@ -75,28 +104,35 @@ if __name__ in {"__main__", "__mp_main__"}:
     else:
         print("Arduino is Disconnected!")
     try:
+        ui.html('<style>.multi-line-notification { font-size: 40px; white-space: pre-line; }</style>')
         # Get the list of image files from both folders
         image_files_a = [filename for filename in os.listdir(folder_a_path) if filename.endswith((".jpg", ".png"))]
         image_files_b = [filename for filename in os.listdir(folder_b_path) if filename.endswith((".jpg", ".png"))]
         print("got", str(len(image_files_a))," files on A")
         print("got", str(len(image_files_b))," files on B")
+        first_random = random.randint(0, min(len(image_files_a), len(image_files_b)) - 1)
+        print(first_random)
+        
+        # Build the UI
         with ui.row().classes('w-full justify-center'):
             ui.label('GeekCon 2023 Trolley Problem Game').classes('text-h4')
         ui.separator()
-        first_random = random.randint(0, min(len(image_files_a), len(image_files_b)) - 1)
-        print(first_random)
+        slider = ui.slider(min=0, max=10, value=0)
+        countdown = ui.timer(descision_timeout_increment, lambda: update_timer())#select_image('NONE'))#, once=True)
+        
         selected_image_a, selected_image_b, text_a, text_b = refresh_choices(first_random, image_files_a, image_files_b)
-        grid = ui.grid(columns=2)#.classes('w-full') #.classes('w-full absolute-center')
+
+        grid = ui.grid(columns=2)
         display_index(first_random, selected_image_a, selected_image_b, text_a, text_b)
         
         with ui.row().classes('w-full justify-center'):
-            # ui.label('GeekCon 2023 Trolley Problem Game').classes('text-h4')
             dark = ui.dark_mode()
             with ui.grid(columns=3).classes('absolute-bottom'):
                 ui.label('Switch mode:')
                 ui.button('Dark', on_click=dark.enable)
                 ui.button('Light', on_click=dark.disable)
         ui.run(port=5000)
+
     except KeyboardInterrupt:
             print("Exiting...")
             arduino.close_connection()
